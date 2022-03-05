732c057e30ab558a8a97779f1d20cb4ab19f7fc0
Josuha100 <101005379+Josuha100@users.noreply.github.com>
2022-03-05 11:20:28 -0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "2+21=?",
+    answer: "23",
+    options: [
+      "21",
+      "221",
+      "23",
+      "19"
+    ]
+  },
+    {
+    numb: 2,
+    question: "3029+102=?",
+    answer: "3131",
+    options: [
+      "3131",
+      "31031",
+      "3030",
+      "2031"
+    ]
+  },
+    {
+    numb: 3,
+    question: "If the perimeter of a square is 3.5cm.What is the length of that square?",
+    answer: "0.875cm",
+    options: [
+      "875cm",
+      "0.8075cm",
+      "0.0875cm",
+      "0.875cm"
+    ]
+  },
+    {
+    numb: 4,
+    question: "-15+31=?",
+    answer: "16",
+    options: [
+      "15",
+      "46",
+      "21",
+      "16"
+    ]
+  },
+    {
+    numb: 5,
+    question: "If Bob has x candies and Joe has 10 candies.In total they have 55 candies but lost 21 candies by ants.How much candies does Bob has?",
+    answer: "24 candies",
+    options: [
+      "31 candies",
+      "31 gum",
+      "24 candies",
+      "55 candies"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
